7571080f922f47af73d7610a479ef4ec0bca5727
Matt Thomas <matt@betweenbrain.com>
2014-09-04 10:19:16 -0400

    Scaffold basic files

---
+++ mod_languagefiletests.php
@@ -0,0 +1,11 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       mod_languagefiletests.php
+ * Created    9/4/14 10:07 AM 
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v2 or later
+ */
+
